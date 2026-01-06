f798eaef1484bffef7b2128da0a76bd1ae15475d
Liladar Jangid <jangidrohit5820@gmail.com>
2026-01-06 21:00:46 +0530

    Student Course Registration System - initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// 🔹 MongoDB Atlas Connection (CLOUD)
+mongoose.connect(
+  "mongodb+srv://studentadmin:studentadmin@cluster0.g3gwkfe.mongodb.net/studentDB?retryWrites=true&w=majority"
+)
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
+
+// Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+// Register User
+app.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.redirect("/");
+  } catch (err) {
+    res.send("Error registering user");
+  }
+});
+
+// Login User
+app.post("/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  if (user) {
+    res.redirect("/home");
+  } else {
+    res.send("Invalid Login Credentials");
+  }
+});
+
+// Server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
