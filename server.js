dc1fcf7b0932f0d19a79a7ddcde587367d3a6192
Liladar Jangid <jangidrohit5820@gmail.com>
2026-01-06 21:29:06 +0530

    Updated server for Render deployment

--- server.js
+++ server.js
@@ -10,32 +10,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // 🔹 MongoDB Atlas Connection (CLOUD)
-mongoose.connect(
-  "mongodb+srv://studentadmin:studentadmin@cluster0.g3gwkfe.mongodb.net/studentDB?retryWrites=true&w=majority"
-)
-.then(() => console.log("MongoDB Atlas Connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect(
+    "mongodb+srv://studentadmin:studentadmin@cluster0.g3gwkfe.mongodb.net/studentDB?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Atlas Connected"))
+  .catch((err) => console.log(err));
 
 // Schema
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String
+  password: String,
 });
 
 const User = mongoose.model("User", userSchema);
 
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/login.html"));
+  res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/register.html"));
+  res.sendFile(path.join(__dirname, "views", "register.html"));
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/home.html"));
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
 // Register User
@@ -45,26 +46,32 @@ app.post("/register", async (req, res) => {
     await user.save();
     res.redirect("/");
   } catch (err) {
+    console.error(err);
     res.send("Error registering user");
   }
 });
 
 // Login User
 app.post("/login", async (req, res) => {
-  const user = await User.findOne({
-    email: req.body.email,
-    password: req.body.password
-  });
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-  if (user) {
-    res.redirect("/home");
-  } else {
-    res.send("Invalid Login Credentials");
+    if (user) {
+      res.redirect("/home");
+    } else {
+      res.send("Invalid Login Credentials");
+    }
+  } catch (err) {
+    console.error(err);
+    res.send("Login error");
   }
 });
 
-// Server
-const PORT = 3000;
+// ✅ IMPORTANT: Render uses its own PORT
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
